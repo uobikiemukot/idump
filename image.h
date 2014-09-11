13301911a8cfd54bba1c02b2f8e47936332665fb
uobikiemukot <uobikiemukot@gmail.com>
2014-09-11 14:12:39 +0900

    add some inline functions and rewrite many functions

--- image.h
+++ image.h
@@ -1,21 +1,91 @@
 /* See LICENSE for licence details. */
 /* this header file depends loader.h */
-inline void get_rgb(uint8_t *r, uint8_t *g, uint8_t *b, const uint8_t *data, int channel)
+
+/* inline functions:
+	never access member of struct image directly */
+static inline int get_frame_count(struct image *img)
 {
-	if (channel <= 2) { /* grayscale (+ alpha) */
-		*r = *g = *b = *data;
-	}
-	else { /* r, g, b (+ alpha) */
-		*r = *(data + 0);
-		*g = *(data + 1);
-		*b = *(data + 2);
+	return img->frame_count;
+}
+
+static inline uint8_t *get_current_frame(struct image *img)
+{
+	return img->data[img->current_frame];
+}
+
+static inline int get_current_delay(struct image *img)
+{
+	return img->delay[img->current_frame];
+}
+
+static inline void increment_frame(struct image *img)
+{
+	img->current_frame = (img->current_frame + 1) % img->frame_count;
+}
+
+static inline int get_image_width(struct image *img)
+{
+	return img->width;
+}
+
+static inline int get_image_height(struct image *img)
+{
+	return img->height;
+}
+
+static inline int get_image_channel(struct image *img)
+{
+	return img->channel;
+}
+
+static inline void get_rgb(struct image *img, uint8_t *data, int x, int y, uint8_t *r, uint8_t *g, uint8_t *b)
+{
+	uint8_t *ptr;
+
+	ptr = data + img->channel * (y * img->width + x);
+
+	if (img->channel <= 2) { /* grayscale (+ alpha) */
+		*r = *g = *b = *ptr;
+	} else {                 /* rgb (+ alpha) */
+		*r = *ptr; *g = *(ptr + 1); *b = *(ptr + 2);
 	}
 }
 
-void rotate_image(struct image *img, int angle)
+static inline void get_average(struct image *img, uint8_t *data, int x_from, int y_from, int x_to, int y_to, uint8_t pixel[])
 {
-	int x1, x2, y1, y2, r, src_width;
-	uint8_t *rotate_data, *dst, *src;
+	int cell_num;
+	uint8_t r, g, b;
+	uint16_t rsum, gsum, bsum;
+
+	rsum = gsum = bsum = 0;
+	for (int y = y_from; y < y_to; y++) {
+		for (int x = x_from; x < x_to; x++) {
+			get_rgb(img, data, x, y, &r, &g, &b);
+			rsum += r; gsum += g; bsum += b;
+		}
+	}
+
+	cell_num = (y_to - y_from) * (x_to - x_from);
+	if (cell_num > 1) {
+		rsum /= cell_num; gsum /= cell_num; bsum /= cell_num;
+	}
+
+	if (img->channel <= 2)
+		*pixel++ = rsum;
+	else {
+		*pixel++ = rsum; *pixel++ = gsum; *pixel++ = bsum;
+	}
+
+	if (img->alpha)
+		*pixel = 0;
+}
+
+/* some image proccessing functions:
+	never use *_single functions directly */
+uint8_t *rotate_image_single(struct image *img, uint8_t *data, int angle)
+{
+	int x1, x2, y1, y2, r, dst_width, dst_height;
+	uint8_t *rotated_data;
 	long offset_dst, offset_src;
 
 	static const int cos[3] = {0, -1,  0};
@@ -29,81 +99,63 @@ void rotate_image(struct image *img, int angle)
 	};
 
 	if (angle != 90 && angle != 180 && angle != 270)
-		return;
+		return NULL;
 	/* r == 0: clockwise        : (angle 90)  */
 	/* r == 1: upside down      : (angle 180) */
 	/* r == 2: counter clockwise: (angle 270) */
 	r = angle / 90 - 1;
 	
-	src_width = img->width;
-	if (angle == 90 || angle == 270)
-		swapint(&img->width, &img->height);
+	if (angle == 90 || angle == 270) {
+		dst_width  = img->height;
+		dst_height = img->width;
+	} else {
+		dst_width  = img->width;
+		dst_height = img->height;
+	}
 
-	rotate_data = (uint8_t *) ecalloc(img->width * img->height, img->channel);
+	if ((rotated_data = (uint8_t *) ecalloc(dst_width * dst_height, img->channel)) == NULL)
+		return NULL;
 
-	src = img->data;
-	dst = rotate_data;
+	logging(DEBUG, "rotated image: %dx%d size:%d\n",
+		dst_width, dst_height, dst_width * dst_height * img->channel);
 
 	for (y2 = 0; y2 < img->height; y2++) {
 		for (x2 = 0; x2 < img->width; x2++) {
 			x1 = ((x2 - shift[r][0]) * cos[r] - (y2 - shift[r][1]) * sin[r]) * shift[r][2];
 			y1 = ((x2 - shift[r][0]) * sin[r] + (y2 - shift[r][1]) * cos[r]) * shift[r][2];
-			offset_src = img->channel * (y1 * src_width + x1);
-			offset_dst = img->channel * (y2 * img->width + x2);
-			memcpy(dst + offset_dst, src + offset_src, img->channel);
+			offset_src = img->channel * (y1 * img->width + x1);
+			offset_dst = img->channel * (y2 * dst_width + x2);
+			memcpy(rotated_data + offset_dst, data + offset_src, img->channel);
 		}
 	}
+	free(data);
 
-	free(img->data);
-	img->data = rotate_data;
+	img->width  = dst_width;
+	img->height = dst_height;
 
-	logging(DEBUG, "rotated image: %dx%d size:%d\n",
-		img->width, img->height, img->width * img->height * img->channel);
+	return rotated_data;
 }
 
-inline void get_average(struct image *img, int w_from, int w_to, int h_from, int h_to,
-	int row_stride, uint8_t *pixel)
+void rotate_image(struct image *img, int angle, bool rotate_all)
 {
-	int h, w, cells;
-	uint8_t r, g, b, *ptr;
-	uint16_t rsum, gsum, bsum;
+	uint8_t *rotated_data;
 
-	rsum = gsum = bsum = 0;
-	for (h = h_from; h < h_to; h++) {
-		for (w = w_from; w < w_to; w++) {
-			ptr = img->data + img->channel * (h * row_stride + w);
-			//get_rgb(&r, &g, &b, ptr, img->channel, img->alpha);
-			get_rgb(&r, &g, &b, ptr, img->channel);
-			rsum += r; gsum += g; bsum += b;
-		}
+	if (rotate_all) {
+		for (int i = 0; i < img->frame_count; i++)
+			if ((rotated_data = rotate_image_single(img, img->data[i], angle)) != NULL)
+				img->data[i] = rotated_data;
+	} else {
+		if ((rotated_data = rotate_image_single(img, img->data[img->current_frame], angle)) != NULL)
+			img->data[img->current_frame] = rotated_data;
 	}
-	cells = (h_to - h_from) * (w_to - w_from);
-	/*
-	logging(DEBUG, "h_from:%d h_to:%d w_from:%d w_to:%d cells:%d\n",
-		h_from, h_to, w_from, w_to, cells);
-	*/
-	if (cells > 1) {
-		rsum /= cells; gsum /= cells; bsum /= cells;
-	}
-
-	if (img->channel <= 2)
-		*pixel++ = rsum;
-	else {
-		*pixel++ = rsum;
-		*pixel++ = gsum;
-		*pixel++ = bsum;
-	}
-
-	if (img->alpha)
-		*pixel = 0;
 }
 
-void resize_image(struct image *img, int disp_width, int disp_height)
+uint8_t *resize_image_single(struct image *img, uint8_t *data, int disp_width, int disp_height)
 {
 	/* TODO: support enlarge */
 	int width_rate, height_rate, resize_rate;
-	int w, h, src_width, h_from, w_from, h_to, w_to;
-	uint8_t *dst, *resized_data, pixel[img->channel];
+	int dst_width, dst_height, y_from, x_from, y_to, x_to;
+	uint8_t *resized_data, pixel[img->channel];
 	long offset_dst;
 
 	width_rate  = MULTIPLER * disp_width  / img->width;
@@ -113,51 +165,117 @@ void resize_image(struct image *img, int disp_width, int disp_height)
 	logging(DEBUG, "width_rate:%.2d height_rate:%.2d resize_rate:%.2d\n",
 		width_rate, height_rate, resize_rate);
 
+	/* only support shrink */
 	if ((resize_rate / MULTIPLER) >= 1)
-		return;
+		return NULL;
 
-	src_width    = img->width;
 	/* FIXME: let the same num (img->width == fb->width), if it causes SEGV, remove "+ 1" */
-	img->width   = resize_rate * img->width / MULTIPLER + 1;
-	img->height  = resize_rate * img->height / MULTIPLER;
-	resized_data = (uint8_t *) ecalloc(img->width * img->height, img->channel);
+	dst_width  = resize_rate * img->width / MULTIPLER + 1;
+	dst_height = resize_rate * img->height / MULTIPLER;
+
+	if ((resized_data = (uint8_t *) ecalloc(dst_width * dst_height, img->channel)) == NULL)
+		return NULL;
 
 	logging(DEBUG, "resized image: %dx%d size:%d\n",
-		img->width, img->height, img->width * img->height * img->channel);
+		dst_width, dst_height, dst_width * dst_height * img->channel);
 
-	dst = resized_data;
-	for (h = 0; h < img->height; h++) {
-		h_from = MULTIPLER * h / resize_rate;
-		h_to   = MULTIPLER * (h + 1) / resize_rate;
-		for (w = 0; w < img->width; w++) {
-			w_from = MULTIPLER * w / resize_rate;
-			w_to   = MULTIPLER * (w + 1) / resize_rate;
-			get_average(img, w_from, w_to, h_from, h_to, src_width, pixel);
-			offset_dst = img->channel * (h * img->width + w);
-			memcpy(dst + offset_dst, pixel, img->channel);
+	for (int x = 0; x < img->height; x++) {
+		x_from = MULTIPLER * x / resize_rate;
+		x_to   = MULTIPLER * (x + 1) / resize_rate;
+		for (int y = 0; y < img->width; y++) {
+			y_from = MULTIPLER * y / resize_rate;
+			y_to   = MULTIPLER * (y + 1) / resize_rate;
+			get_average(img, data, x_from, y_from, x_to, y_to, pixel);
+			offset_dst = img->channel * (y * dst_width + x);
+			memcpy(resized_data + offset_dst, pixel, img->channel);
 		}
 	}
-	free(img->data);
-	img->data = resized_data;
+	free(data);
+
+	img->width  = dst_width;
+	img->height = dst_height;
+
+	return resized_data;
 }
 
-void draw_single(struct framebuffer *fb, struct image *img,
+void resize_image(struct image *img, int disp_width, int disp_height, bool resize_all)
+{
+	uint8_t *resized_data;
+
+	if (resize_all) {
+		for (int i = 0; i < img->frame_count; i++)
+			if ((resized_data = resize_image_single(img, img->data[i], disp_width, disp_height)) != NULL)
+				img->data[i] = resized_data;
+	} else {
+		if ((resized_data = resize_image_single(img, img->data[img->current_frame], disp_width, disp_height)) != NULL)
+			img->data[img->current_frame] = resized_data;
+	}
+}
+
+uint8_t *normalize_bpp_single(struct image *img, uint8_t *data, int bytes_per_pixel)
+{
+	uint8_t *normalized_data, *src, *dst, r, g, b;
+
+	if ((normalized_data = (uint8_t *)
+		ecalloc(img->width * img->height, bytes_per_pixel)) == NULL)
+		return NULL;
+
+	if (img->channel <= 2) { /* grayscale (+ alpha) */
+		for (int y = 0; y < img->height; y++) {
+			for (int x = 0; x < img->width; x++) {
+				src = data + img->channel * (y * img->width + x);
+				dst = normalized_data + bytes_per_pixel * (y * img->width + x);
+				*dst = *src; *(dst + 1) = *src; *(dst + 2) = *src;
+			}
+		}
+	} else {                 /* rgb (+ alpha) */
+		for (int y = 0; y < img->height; y++) {
+			for (int x = 0; x < img->width; x++) {
+				get_rgb(img, data, x, y, &r, &g, &b);
+				dst = normalized_data + bytes_per_pixel * (y * img->width + x);
+				*dst = r; *(dst + 1) = g; *(dst + 2) = b;
+			}
+		}
+	}
+	free(data);
+
+	return normalized_data;
+}
+
+void normalize_bpp(struct image *img, int bytes_per_pixel, bool normalize_all)
+{
+	uint8_t *normalized_data;
+
+	/* XXX: now only support bytes_per_pixel == 3 */
+	if (bytes_per_pixel != 3)
+		return;
+
+	if (normalize_all) {
+		for (int i = 0; i < img->frame_count; i++)
+			if ((normalized_data = normalize_bpp_single(img, img->data[i], bytes_per_pixel)) != NULL)
+				img->data[i] = normalized_data;
+	} else {
+		if ((normalized_data = normalize_bpp_single(img, img->data[img->current_frame], bytes_per_pixel)) != NULL)
+			img->data[img->current_frame] = normalized_data;
+	}
+}
+
+void draw_single(struct framebuffer *fb, struct image *img, uint8_t *data,
 	int offset_x, int offset_y, int shift_x, int shift_y, int width, int height)
 {
-	int w, h, offset, size;
-	uint8_t r, g, b, *ptr;
+	int offset, size;
+	uint8_t r, g, b;
 	uint32_t color;
 
-	for (h = 0; h < height; h++) {
+	for (int h = 0; h < height; h++) {
 		if (h >= fb->height)
 			break;
 
-		for (w = 0; w < width; w++) {
+		for (int w = 0; w < width; w++) {
 			if (w >= fb->width)
 				break;
 
-			ptr = img->data + img->channel * ((h + shift_y) * img->width + w + shift_x);
-			get_rgb(&r, &g, &b, ptr, img->channel);
+			get_rgb(img, data, w + shift_x, h + shift_y, &r, &g, &b);
 			color = get_color(&fb->vinfo, r, g, b);
 
 			/* update copy buffer */
@@ -177,27 +295,6 @@ void draw_single(struct framebuffer *fb, struct image *img,
 		size *= fb->line_length;
 		memcpy(fb->fp, fb->buf, size);
 	}
-}
-
-void draw_anim(struct framebuffer *fb, struct image *img,
-	int offset_x, int offset_y, int shift_x, int shift_y, int width, int height, bool enable_anim)
-{
-	unsigned int loop_count = 0;
-
-	if (enable_anim) { /* ignore img->loop_count, force 1 loop  */
-		while (loop_count < img->frame_count) {
-			img->data = img->anim[loop_count];
-			draw_single(fb, img, offset_x, offset_y, shift_x, shift_y, width, height);
-			usleep(img->delay[loop_count] * 10000); /* gif delay 1 == 1/100 sec */
-			loop_count++;
-		}
-	} else {           /* draw img->current_frame only */
-		img->data = img->anim[img->current_frame % img->frame_count];
-		draw_single(fb, img, offset_x, offset_y, shift_x, shift_y, width, height);
-	}
-
-	/* must be reset pointer to avoid double free */
-	img->data = NULL;
 }
 
 void draw_image(struct framebuffer *fb, struct image *img,
@@ -228,6 +325,7 @@ void draw_image(struct framebuffer *fb, struct image *img,
 		|       <-  width ->           |
 		+------------------------------+
 	*/
+	int loop_count = 0;
 
 	if (shift_x + width > img->width)
 		width = img->width - shift_x;
@@ -241,10 +339,14 @@ void draw_image(struct framebuffer *fb, struct image *img,
 	if (offset_y + height > fb->height)
 		height = fb->height - offset_y;
 
-	if (enable_anim && img->anim)
-		draw_anim(fb, img, offset_x, offset_y, shift_x, shift_y, width, height, enable_anim);
-	else if (img->data)
-		draw_single(fb, img, offset_x, offset_y, shift_x, shift_y, width, height);
-	else
-		logging(ERROR, "img->anim and img->data are NULL\n");
+	/* XXX: ignore img->loop_count, force 1 loop */
+	if (enable_anim) {
+		while (loop_count < img->frame_count) {
+			draw_single(fb, img, img->data[loop_count], offset_x, offset_y, shift_x, shift_y, width, height);
+			usleep(img->delay[loop_count] * 10000); /* gif delay 1 == 1/100 sec */
+			loop_count++;
+		}
+	} else {
+		draw_single(fb, img, img->data[img->current_frame], offset_x, offset_y, shift_x, shift_y, width, height);
+	}
 }
